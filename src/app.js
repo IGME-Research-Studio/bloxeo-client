bc69199e08b62c3802eb1298d5809b65fa6ce485
Peter Gyory <ptg3662@rit.edu>
2015-09-20 19:16:00 -0400

    Fix errors in demo app.js file

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const React = require('react');
 
 const HelloMessage = React.createClass({
-  render: () => {
+  render: function () {
     return <div>Hello {this.props.name}</div>;
   },
 });
 
-React.render(<HelloMessage name="John" />, mountNode);
+const body = document.querySelector('body');
+
+React.render(<HelloMessage name="John" />, body);

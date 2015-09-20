20b28d4ad7d80daef06983a6e47e1839d754e5b0
Will <whp3652@rit.edu>
2015-09-20 16:22:02 -0400

    Setup ES6, Browserify, linting, and testing

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-var React = require('react');
+const React = require('react');
+
+const HelloMessage = React.createClass({
+  render: () => {
+    return <div>Hello {this.props.name}</div>;
+  },
+});
+
+React.render(<HelloMessage name="John" />, mountNode);

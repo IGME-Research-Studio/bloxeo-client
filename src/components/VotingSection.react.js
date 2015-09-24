9b6abfe2af25f3907294c0d90ebae24b96b3aa10
Peter Gyory <ptg3662@rit.edu>
2015-09-24 16:57:16 -0400

    Move Voting section classes to separate files

--- src/components/VotingSection.react.js
+++ src/components/VotingSection.react.js
@@ -1,89 +1,41 @@
 const React = require('react');
+const VotingResult = require('../components/VotingResult.react');
+const VoteButton = require('../components/VoteButton.react');
+const VoteElement = require('../components/VoteElement.react');
 
-let i = 0;
 // page for displaying ideas and voting on them
 const VotingSection = React.createClass({
   // set state to the first element of the array
   getInitialState: function() {
     return (
-      {currentIdea: this.props.data[i].idea}
+      {
+        currentIdea: this.props.data[0].content,
+        votesCast: 0
+      }
     );
   },
   // changes state on button click
   handleStateChange: function(keep) {
     if (!keep) {
-      this.props.data[i].keep = false;
+      this.props.data[this.state.votesCast].keep = false;
     }
 
-    i++;
-    if (i === this.props.data.length) {
+    this.state.votesCast++;
+    if (this.state.votesCast === this.props.data.length) {
       const body = document.querySelector('body');
       React.render(<VotingResult data={this.props.data} />, body);
     } else {
-      this.setState({currentIdea: this.props.data[i].idea});
+      this.setState({currentIdea: this.props.data[this.state.votesCast].content});
     }
   },
   render: function() {
     return (
       <div>
-        <Idea idea={this.state.currentIdea} />
+        <VoteElement idea={this.state.currentIdea} />
         <VoteButton data="true" changeState={this.handleStateChange} />
         <VoteButton data="false" changeState={this.handleStateChange} />
       </div>
     );
-  },
-});
-
-// page to display voting results
-const VotingResult = React.createClass({
-  render: function() {
-    // array for ideas that were voted no
-    const droppedIdeas = [];
-    // map the updated idea array
-    const keepIdeas = this.props.data.map(function(keepIdea) {
-      // if keep is true, return idea to keepIdeas
-      if (!keepIdea.keep) {
-        // if the idea is false, push it to the droppedIdeas array
-        const dropIdea = <Idea idea={keepIdea.idea} />;
-        droppedIdeas.push(dropIdea);
-      } else {
-        return (
-          <Idea idea={keepIdea.idea} />
-        );
-      }
-    });
-    return (
-      <div>
-        <h1> Ideas to keep </h1>
-          {keepIdeas}
-        <h1> Ideas to forget </h1>
-          {droppedIdeas}
-        <a className="voteButton">Back to Main Screen</a>
-      </div>
-    );
-  },
-});
-
-const Idea = React.createClass({
-  render: function() {
-    return (
-      <div>
-        {this.props.idea}
-      </div>
-    );
-  },
-});
-
-const VoteButton = React.createClass({
-  handleClick: function(e) {
-    e.preventDefault();
-    this.props.changeState(this.props.data === 'true');
-  },
-  render: function() {
-    if (this.props.data === 'true') {
-      return <button className="yesButton" onClick={this.handleClick}>Yes</button>;
-    }
-    return <button className="noButton" onClick={this.handleClick}>No</button>;
   },
 });
 

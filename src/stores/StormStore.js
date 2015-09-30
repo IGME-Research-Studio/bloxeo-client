bb6f38d2a10023b93953f86f7a3b5ea9844bb265
Peter Gyory <ptg3662@rit.edu>
2015-09-30 19:12:35 -0400

    Add base storm store file

---
+++ src/stores/StormStore.js
@@ -0,0 +1,35 @@
+const AppDispatcher = require('../dispatcher/AppDispatcher');
+const StormConstants = require('../constants/StormConstants');
+const EventEmitter = require('events').EventEmitter;
+const assign = require('object-assign');
+
+const CHANGE_EVENT = 'change';
+
+const ideas = [];
+
+function create () {
+  //Push an idea to array
+};
+
+const StormStore = assign({}, EventEmitter.prototype, {
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+});
+
+AppDispatcher.register(function(action) {
+  switch(action.actionType) {
+    case StormConstants.IDEA_CREATE:
+      break;
+  }
+});
+
+module.exports = StormStore;

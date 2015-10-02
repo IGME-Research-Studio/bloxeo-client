c1866774c539813e2a9eccb241b638f751af5715
Ashton Wai <indexsky@users.noreply.github.com>
2015-10-02 15:38:31 -0400

    Change component SidebarRoomName to RoomName.

--- src/components/RoomName.react.js
+++ src/components/RoomName.react.js
@@ -1,16 +1,16 @@
 const React = require('react');
 
-const SidebarRoomName = React.createClass({
+const RoomName = React.createClass({
   /**
    * @return {object}
    */
   render: function() {
     return (
-      <div id="roomName">
+      <div>
         <h2>Room Name</h2>
       </div>
     );
   },
 });
 
-module.exports = SidebarRoomName;
+module.exports = RoomName;

d81fbcb8edbfbef6151b3826c29cc1c0857d490b
Cailin Li <cxl2467@rit.edu>
2015-10-26 00:26:50 -0400

    Add trash can and remove idea feature

---
+++ src/components/TrashCan.react.js
@@ -0,0 +1,69 @@
+const React           = require('react');
+const StormActions    = require('../actions/StormActions');
+const dropTarget      = require('react-dnd').DropTarget;
+const PropTypes       = React.PropTypes;
+const DnDTypes        = require('../constants/DragAndDropConstants');
+
+/**
+ * TrashCan component which is a dragTarget for IdeaCollections
+ */
+const TrashCan = React.createClass({
+  propTypes: {
+    connectDropTarget: PropTypes.func.isRequired,
+  },
+  /**
+   * Render TrashCan component as a DragTarget
+   * @return {object}
+   */
+  render: function() {
+    const connectDropTarget = this.props.connectDropTarget;
+    return connectDropTarget(
+      <img className="trashCan" src="assets/trashCanIcon.png" />
+    );
+  },
+});
+
+/**
+ * DrogTarget Parameters
+ */
+const collectionTarget = {
+  /**
+   * Only collections with a single idea can be trashed
+   * @param {object} props
+   * @param (object) monitor
+   */
+  canDrop: function(props, monitor) {
+    const idea = monitor.getItem();
+    return (idea.ideaCount === 1);
+  },
+  /**
+   * Remove the collection dropped onto the trash can
+   * @param {object} props
+   * @param (object) monitor
+   */
+  drop: function(props, monitor) {
+    const idea = monitor.getItem();
+
+    // Remove the dropped collection
+    if (idea.type === DnDTypes.COLLECTION) {
+      StormActions.removeCollection(idea.id);
+    }
+  },
+};
+
+/**
+ * Returns an object of the props to inject into this DragTarget component
+ * @param {object} connect
+ * @return {object} - objected injected into component
+ */
+function targetCollect(connect) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+  };
+}
+
+module.exports = dropTarget(
+  DnDTypes.COLLECTION,
+  collectionTarget,
+  targetCollect
+)(TrashCan);

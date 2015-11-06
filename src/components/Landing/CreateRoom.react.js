f70cf01c714ebfaca867eb64dfca89c61dd6734f
Ashton Wai <indexsky@users.noreply.github.com>
2015-11-06 16:17:55 -0500

    Add create new board function.
    User join board after creating new board.
    
    Direct to workspace.
    
    Resolve conflicts.

--- src/components/Landing/CreateRoom.react.js
+++ src/components/Landing/CreateRoom.react.js
@@ -1,11 +1,16 @@
 const React = require('react');
-const Link  = require('react-router').Link;
+// const Link  = require('react-router').Link;
+const StormActions = require('../../actions/StormActions');
 
 const CreateRoom = React.createClass({
+  _onClick: function() {
+    StormActions.createBoard();
+    window.location = '/#/workSpace';
+  },
   render: function() {
     return (
       <div className="createRoomButton">
-        <Link to="/workSpace" className="button">Create a room</Link>
+        <a className="button" onClick={this._onClick}>Create a room</a>
       </div>
     );
   },

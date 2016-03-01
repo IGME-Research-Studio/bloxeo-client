7d68c3498895db462c56acb1f4cd1fa42bbb96c7
Will <whp3652@rit.edu>
2016-03-01 01:39:52 -0500

    Implement material-ui buttons

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import { render } from 'react-dom';
 import { browserHistory } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import renderRoutes from './routes.react';
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 window.onload = () => {
   render(renderRoutes(browserHistory), document.querySelector('#mount'));
